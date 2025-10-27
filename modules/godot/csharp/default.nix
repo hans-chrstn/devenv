601267d60a4064b3e3a9e0167e81986a36937486
hayato-oo <xuhiko13@gmail.com>
2025-10-27 10:33:06 -0400

    use godot-mono as default

--- modules/godot/csharp/default.nix
+++ modules/godot/csharp/default.nix
@@ -4,16 +4,10 @@ let
   libs = with pkgs; [
     omnisharp-roslyn
   ];
-  godotShell = import ../default.nix {inherit pkgs; };
+  godotShell = import ../default.nix { inherit pkgs; };
 in
-pkgs.mkShell rec {
+pkgs.mkShell {
+  inherit (godotShell) shellHook nativeBuildInputs;
   buildInputs = godotShell.buildInputs ++ libs;
-  nativeBuildInputs = with pkgs; [
-    aseprite
-    godot-mono
-  ];
-  LD_LIBRARY_PATH = pkgs.lib.strings.makeLibraryPath buildInputs;
   DOTNET_ROOT = "${pkgs.dotnet-sdk_9}";
-  shellHook = ''
-  '';
 }
--- modules/godot/default.nix
+++ modules/godot/default.nix
@@ -3,18 +3,25 @@ let
   libs = with pkgs; [
     dotnet-sdk_9
   ];
+
+  godotAlias = pkgs.writeShellScriptBin "godot" ''
+    exec ${pkgs.godot-mono}/bin/godot-mono "$@"
+  '';
 in
 pkgs.mkShell {
-  buildInputs = libs;
-  nativeBuildInputs = with pkgs; [
-    godot
-    aseprite
-  ];
+  buildInputs =
+    libs
+    ++ (with pkgs; [
+      godot-mono
+      aseprite
+      godotAlias
+    ]);
   LD_LIBRARY_PATH = pkgs.lib.strings.makeLibraryPath libs;
-  GODOT_FILE = "${pkgs.godot}";
+  DOTNET_ROOT = "${pkgs.dotnet-sdk_9}";
+  GODOT_FILE = "${pkgs.godot-mono}";
   shellHook = ''
-      echo "To run godot with editor, do godot -e"
-      echo "To run aseprite, do aseprite"
-      echo "To run VSCode, do code"
+    echo "To run godot with project manager, do godot -p"
+    echo "To run aseprite, do aseprite"
+    echo "To run VSCode, do code"
   '';
 }
